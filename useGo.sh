bae6b8bd10633963b9f6d2269e0bb588e2e4a579
Dirk Strauss <dstrauss@ds-2.de>
2018-03-07 15:49:37 +0100

    - somehow it works ...

---
+++ useGo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#export GOPATH=/Users/dstrauss/git/ds2-golang-lib::/Users/dstrauss/git/ds2-golang-lib/src
+export GOPATH=`pwd`
+export GOBIN=`pwd`/shellbin
+export PKGDIR=`pwd`/pkg
+
+echo "Perform cleanup"
+go clean
+rm -r pkg 2>>/dev/null
+rm -r bin 2>>/dev/null
+# echo "Formatting the code.."
+# go fmt github.com/ds2/golang/sysinfo
+echo "GETting the imports.."
+go get -pkgdir $PKGDIR -v github.com/ds2/golang/sysinfo
+# echo "Building final stuff"
+#go build -pkgdir $PKGDIR -v -x -buildmode=archive github.com/ds2/golang/sysinfo
+# go build -v -x -buildmode=archive github.com/ds2/golang/sysinfo
+echo "Installing it"
+go install -pkgdir $PKGDIR -v github.com/ds2/golang/sysinfo
+echo "Done"

5f6a0ca4555e6f1499ad7a2f7aee439f9eaf1a5b
Dirk Strauss <dstrauss@ds-2.de>
2018-03-12 07:38:05 +0100

    - current snapshot

--- useGo.sh
+++ useGo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export GOPATH=/Users/dstrauss/git/ds2-golang-lib::/Users/dstrauss/git/ds2-golang-lib/src
-export GOPATH=`pwd`
+export GOPATH=`pwd`:`pwd`/sysinfo
 export GOBIN=`pwd`/shellbin
 export PKGDIR=`pwd`/pkg
 
@@ -12,13 +12,13 @@ rm -r bin 2>>/dev/null
 # echo "Formatting the code.."
 # go fmt github.com/ds2/golang/sysinfo
 echo "GETting the imports.."
-go get -pkgdir $PKGDIR -v github.com/ds2/golang/sysinfo
-# echo "Building final stuff"
-#go build -pkgdir $PKGDIR -v -x -buildmode=archive github.com/ds2/golang/sysinfo
+go get -pkgdir $PKGDIR -v ./...
+echo "Building final stuff"
+go build -pkgdir $PKGDIR -v -x -buildmode=archive ./...
 # go build -v -x -buildmode=archive github.com/ds2/golang/sysinfo
 go test -run ''
 echo "Installing it"
-go install -pkgdir $PKGDIR -v github.com/ds2/golang/sysinfo
+go install -pkgdir $PKGDIR -v ./...
 echo "Run Doc server.."
 godoc -http=:6060
 echo "Done"

adc5642a4fab376887633179142eeb243336afcd
Dirk Strauss <dstrauss@ds-2.de>
2020-03-25 16:03:37 +0100

    - idea to perform k8s calls

--- k8s/nodes.go
+++ k8s/nodes.go
@@ -82,3 +82,37 @@ func GetNodesInternalIpAddresses(clusterEndpoint string, limit uint, authToken s
 	logrus.Debug("Result: ", result)
 	return result
 }
+
+//func homeDir() string {
+//	if h := os.Getenv("HOME"); h != "" {
+//		return h
+//	}
+//	return os.Getenv("USERPROFILE") // windows
+//}
+//
+//func rolloutNewImageToDeployment(details RolloutImageDetails) {
+//	var kubeconfig *string
+//	if home := homeDir(); home != "" {
+//		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+//	} else {
+//		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+//	}
+//	flag.Parse()
+//
+//	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//
+//	// create the clientset
+//	clientset, err := kubernetes.NewForConfig(config)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//	deploymentData, err:=clientset.AppsV1().Deployments(details.Namespace).Get(details.DeploymentName, metav1.GetOptions{})
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//
+//
+//}

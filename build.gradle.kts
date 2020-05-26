dca8ebeed3af037e96d83d04ee73e9f24512e801
Dirk Strauss <dstrauss@ds-2.de>
2020-05-26 15:55:27 +0200

    - template

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ tasks.register<com.pivotstir.gogradle.tasks.GoClean>("clean") {
 }
 
 release {
+    tagTemplate = "v\${version}"
     with(propertyMissing("git") as net.researchgate.release.GitAdapter.GitConfig) {
         requireBranch = "release/2020"
     }

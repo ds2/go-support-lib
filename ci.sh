d46d3c943ca8c3a51212e9c7126d335f55e235ac
Dirk Strauss <472167-dstrauss@users.noreply.gitlab.com>
2022-06-17 08:37:26 +0200

    - install protoc via ci script

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+apt-get update
+apt-get install -y protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 protoc --go_out=. common/*.proto
 protoc --go_out=. docker/*.proto
